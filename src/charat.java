45c40b5dfb7628bc5ac3788842aea46cbce1eefd
shaikmohammedraheer <124768355+shaikmohammedraheer@users.noreply.github.com>
2023-02-08 13:55:18 +0530

    Initial commit

---
+++ src/charat.java
@@ -0,0 +1,6 @@
+public class charat{
+    public static void main(String args[]){
+        String name="raheer";
+        char ch=name.charAt(4);//returns the char value at the 4th index
+        System.out.println(ch);
+    }}
---
+++ src/endwith.java
@@ -0,0 +1,6 @@
+public class endwith{
+    public static void main(String args[]){
+        String s1="java by javatpoint";
+        System.out.println(s1.endsWith("t"));
+        System.out.println(s1.endsWith("point"));
+    }}
